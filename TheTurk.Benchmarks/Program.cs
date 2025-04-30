415c55b804f0a8f0069c9c1170796d924053fd0d
YakupIpek <yakupipek@gmail.com>
2025-04-30 17:41:17 +0400

    Benchmark for move generation

--- TheTurk.Benchmarks/Program.cs
+++ TheTurk.Benchmarks/Program.cs
@@ -28,4 +28,49 @@ public class MyBenchmark
             UCIProtocol.WriteOutput(item);
         }
     }
+
+    public record PerftCase(string Name, string FEN, int[] ExpectedCounts);
+
+    [ParamsSource(nameof(TestCases))]
+    public PerftCase TestCase;
+
+    public static IEnumerable<PerftCase> TestCases =>
+    [
+        new PerftCase("Starting Position", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", [20, 400, 8902, 197281, 4865609, 119060324]),
+        new PerftCase("Test Position 1", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", [48, 2039, 97862, 4085603, 193690690]),
+        new PerftCase("Test Position 2", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", [14, 191, 2812, 43238, 674624, 11030083])
+    ];
+
+    public void PerftTest()
+    {
+        var (testName, fen, movesCount) =  TestCase;
+
+        var board = Notation.GetBoardState(fen);
+
+        foreach (var (i, moveCount) in movesCount.Index())
+        {
+            if (moveCount != MinMax(board, i + 1))
+                throw new Exception($"Test failed for {testName} at depth {i + 1}");
+        }
+    }
+
+    public static int MinMax(BoardState boardCurrent, int ply)
+    {
+        if (ply == 0)
+            return 1;
+
+        var moves = new MoveGen(boardCurrent).Collect();
+
+        var nodes = 0;
+        foreach (var move in moves)
+        {
+            var board = boardCurrent.Clone();
+
+            if (!board.Play(move))
+                continue;
+
+            nodes += MinMax(board, ply - 1);
+        }
+        return nodes;
+    }
 }
--- TheTurk.Tests/MoveGenerateValidation.cs
+++ TheTurk.Tests/MoveGenerateValidation.cs
@@ -6,10 +6,10 @@ namespace TheTurk.Tests
     public class MoveGenerateValidation
     {
         [DataTestMethod]
-        [DataRow("Starting Position", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", new int[] { 20, 400, 8902, 197281, 4865609, 119060324 }, 5)]
-        [DataRow("Test Position 1", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", new int[] { 48, 2039, 97862, 4085603, 193690690 }, 4)]
-        [DataRow("Test Position 2", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", new int[] { 14, 191, 2812, 43238, 674624, 11030083 }, 5)]
-        public void PerftTest(string testName, string fen, int[] movesCount, int depth)
+        [DataRow("Starting Position", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", new int[] { 20, 400, 8902, 197281, 4865609, 119060324 })]
+        [DataRow("Test Position 1", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", new int[] { 48, 2039, 97862, 4085603, 193690690 })]
+        [DataRow("Test Position 2", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", new int[] { 14, 191, 2812, 43238, 674624, 11030083 })]
+        public void PerftTest(string testName, string fen, int[] movesCount)
         {
             var board = new Board();
 
